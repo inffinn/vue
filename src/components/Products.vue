883eee9b3b4149728c8dbb73ba18b8e8dce230a7
inffinn <inffinn@gmail.com>
2019-05-04 16:08:40 +0300

    first vue

---
+++ src/components/Products.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="products">
+    <table>
+      <thead>
+      <th @click="sort(col)" v-for="col in columns">{{col}}
+      </th>
+      <th>actions</th>
+      </thead>
+      <tr v-for="row in products">
+        <td v-for="item in row">{{item}}
+        </td>
+        <td>
+        <button @click="inc(row)">+</button>
+        <button v-if="row.count>0" @click="dec(row)">-</button>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  import { mapActions, mapState} from 'vuex';
+  export default {
+    name: 'Products',
+    methods:{
+      ...mapActions(['dec','inc','sort'])
+    },
+    computed:{
+      ...mapState(['columns','products','sortData'])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
