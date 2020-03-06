695b11f07e46122596747a1990cad0212ef4c068
inffinn <inffinn@gmail.com>
2020-03-06 16:57:06 +0300

    test

---
+++ src/components/SlotTest.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button v-on:click="decrement">-</button>
+        {{ count }}
+        <button v-on:click="increment">+</button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+  // Define the component in class-style
+  @Component
+export default class Counter extends Vue {
+    // Class properties will be component data
+    count = 0
+
+    // Methods will be component methods
+    increment(): any {
+      // eslint-disable-next-line no-plusplus
+      this.count++;
+    }
+
+    decrement() {
+      // eslint-disable-next-line no-plusplus
+      this.count--;
+    }
+}
+</script>
